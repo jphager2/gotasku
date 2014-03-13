8529e4d2aec90361fee5b309bcbd7abc82fc37b1
jphager2 <jphager2@gmail.com>
2014-03-13 16:04:35 +0100

    working on validate sgf issue when AW comes before AB

--- lib/gotasku/parser.rb
+++ lib/gotasku/parser.rb
@@ -4,8 +4,70 @@ class Gotasku::Parser < SGF::Parser
     super(validate(sgf))	
 	end
 
-	# must return the sgf
+=begin
 	def validate(sgf)
+
+		temp = '<(" <) <( " )> (> ")>'
+
+		index = 0
+
+		# check next part of the string
+		while sgf[index..-1] =~ /AB/
+			puts 'got here 1'
+
+			index = (sgf[index..-1] =~ /AB/)
+
+			this_part = sgf[index..-1]
+
+			ab_scan = this_part.scan(/\[[^W]+\]AB\[..\]/)
+			this_part.gsub!(/\[[^W]+\]AB\[..\]/, temp)
+
+			ab_scan.each do |slice| 
+				slice.gsub!(/(\[..\])AB/) { "#{$1}" }
+				this_part.sub!(temp, slice)
+			end
+
+			puts 'this_part is: ' + this_part
+			sgf[index..-1] = this_part
+
+		  index += 1
+		end
+
+		sgf
+	end
+=end
+
+  def validate(sgf)
+		regs = {black: /AB/, white: /AW/}
+		[:black].each do |color|
+			index = 0
+
+			# check next part of the string
+			while sgf[index..-1] =~ regs[color]
+
+				# get the index of the next occurance
+				index = (sgf[index..-1] =~ regs[color])
+
+				# get substring from index to end
+				this_part = sgf[index..-1]
+
+				# validate the substring
+				validate_part(this_part, color)
+
+				# replace the validated substring
+				sgf[index..-1] = this_part
+
+				# move ahead, so this index won't match again
+				index += 1
+			end
+		end
+
+		sgf
+	end
+
+
+	# must return the sgf
+	def validate_part(sgf, color)
 		# changes bad sgf: AB[xx]AB[xx]AW[xx] => AB[xx][xx]AW[xx]
 		temp = '<(" <) <( " )> (> ")>'
 
