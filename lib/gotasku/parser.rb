89ed72909523d25202665dbf679cfeba085ec438
jphager2 <jphager2@gmail.com>
2014-03-13 18:11:53 +0100

    much better factored, but not sure about how it will handle edge cases

--- lib/gotasku/parser.rb
+++ lib/gotasku/parser.rb
@@ -4,51 +4,27 @@ class Gotasku::Parser < SGF::Parser
     super(validate(sgf))	
 	end
 
-  def validate(sgf)
-		regs = {black: /AB/, white: /AW/}
+	# this is designed to only work for sgf text that has one set of
+	# AB and one set of AW, won't work if there are any other AB or AW
+	# in the middle of the tree
+	def validate(sgf)
+	  validate_for(validate_for(sgf, :AB), :AW)
+	end
 
-		regs.keys.each do |color|
-			index = 0
+	# danger is if there is "AB" in a comment, e.g. "It's ABOUT time!"
+	def validate_for(sgf, property)
+		regs = {AB: /AB/, AW: /AW/}
 
-			# check next part of the string
-			while sgf[index..-1] =~ regs[color]
+		# find the first instance, all will be replaced and the first 
+		# instance will be readded
+		start = sgf =~ regs[property] 
 
-				# get the index of the next occurance
-				index = (sgf[index..-1] =~ regs[color])
-
-				# get substring from index to end
-				this_part = sgf[index..-1]
-
-				# validate the substring
-				validate_part(this_part, color)
-
-				# replace the validated substring
-				sgf[index..-1] = this_part
-
-				# move ahead, so this index won't match again
-				index += 1
-			end
+		if start
+			sgf.gsub!(regs[property], '')
+			sgf = sgf[0...start] + property.to_s + sgf[start..-1]
 		end
 
 		sgf
 	end
 
-	# must return the sgf
-	def validate_part(sgf, color)
-		# changes bad sgf: AB[xx]AB[xx]AW[xx] => AB[xx][xx]AW[xx]
-		temp = '<(" <) <( " )> (> ")>'
-		
-		regs = {black: [/\[[^W]+\]AB\[..\]/, /(\[..\])AB/],
-						white: [/\[[^B]+\]AW/,       /(\[..\])AW/]}
-
-		scan = sgf.scan(regs[color].first)
-		sgf.gsub!(regs[color].first, temp)
-	
-		scan.each do |slice| 
-			slice.gsub!(regs[color].last) { "#{$1}" }
-			sgf.sub!(temp, slice)
-		end
-
-		sgf
-	end
 end
