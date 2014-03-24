b45e187aadf98056585ea87bdc578e5bf9277fd5
jphager2 <jphager2@gmail.com>
2014-03-24 15:39:09 +0100

    added test for query

--- test/cli.rb
+++ test/cli.rb
@@ -1,3 +1,5 @@
+require_relative '../lib/gotasku'
+
 require 'minitest/autorun'
 require 'stringio'
 
---
+++ test/query.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/gotasku'
+
+require 'minitest/autorun'
+
+require 'mongo'
+require 'stringio'
+
+class GotaskuQueryTest < MiniTest::Unit::TestCase
+
+	def test_makes_query
+
+	end
+
+	def test_builds_query
+	end
+
+	def test_handles_big_query
+	end
+end
