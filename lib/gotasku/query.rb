a52669c6e4c297c0790cae443d69c0d9a22ef0eb
jphager2 <jphager2@gmail.com>
2014-03-24 14:53:02 +0100

    gotasku queries, build queries

--- lib/gotasku/query.rb
+++ lib/gotasku/query.rb
@@ -1,10 +1,25 @@
 class Gotasku::Query 
 	@@problems = Mongo::MongoClient.new.db('sgf').collection('problems')
 
-	def initialize
-		query_run
+	attr_reader :found
+
+	# gets a Mongo::Cursor object
+	def initialize(query)
+		@found = @@problems.find(query) 
 	end
 
+	# returns an array of items in @found
+	def to_a 
+		@found.rewind!
+		@found.to_a
+	end
+
+	# returns a Mongo::Cursor object
+	def self.create
+		self.new(self.build_query)
+	end
+
+=begin
 	# key is a string or symbol
 	def self.sort_by(key)
 	end
@@ -12,10 +27,11 @@ class Gotasku::Query
 	# query is a hash 
 	def self.query_by(query)
 	end
+=end
 
 	private
 	  # prompts user for input
-	  def prompt(text, tru = nil, fals = nil)
+		def self.prompt(text, tru = nil, fals = nil)
 			print text
 
 			response = gets.chomp.strip.downcase
@@ -30,20 +46,25 @@ class Gotasku::Query
 					nil
 				end
 			else # else return an Array
-				response = response.gsub(/\s/, '').split(',')
-				unless response.class == Array
+				response = response.split(',')
+
+				# throw an exception if the input is invalid
+				unless response.class == Array and 
+					     not(response.find {|item| item =~ /[a-z]/})
 					raise InvalidEntry
 				end
+
+				response.collect {|item| item.to_i}
 			end
 		end
 
 	  #gets unique set of types from collection
-	  def get_types
+		def self.get_types
 			@@problems.distinct(:type).sort
 		end
 
 		#gets unique set of difficulties from collection
-		def get_difficulties
+		def self.get_difficulties
 			@@problems.distinct(:difficulty).sort do |x, y| 
 				x = Gotasku::DifficultyString.new(x).convert
 				y = Gotasku::DifficultyString.new(y).convert
@@ -52,26 +73,32 @@ class Gotasku::Query
 		end
 		
 	  #gets unique set of ratings from collection
-	  def get_ratings
+		def self.get_ratings
 			@@problems.distinct(:rating).sort
 		end
 
-	  # runs a prompt to query the collection
-	  def query_run
+	  # runs a prompt to build a query for the database
+		def self.build_query
+			query_build = {} 
 			q = {
-						type: -> {get_types},
-						difficulty: -> {get_difficulties},
-						rating: -> {get_ratings}
+						type:       -> {self.get_types},
+						difficulty: -> {self.get_difficulties},
+						rating:     -> {self.get_ratings}
 			    }
 
 			q.keys.each do |key|
-				if prompt("Do you want to filter by #{key} (y/n)? ", 'y', 'n')
-					Gotasku::Display.show(q[key].call)
+				if self.prompt("Do you want to filter by #{key} (y/n)? ", 'y', 'n')
+					list = q[key].call
+					Gotasku::Display.show(list)
 					puts '=' * 40
-					prompt("Please type the index/number for the values " +
-								 "you would like to filter for (e.g. 0, 3, 4)")
+					values = self.prompt("Please type the index/number for the values " +
+								               "you would like to filter for (e.g. 0, 3, 4): ")
+
+					values.collect! {|value| list[value]}
+					query_build[key] = { "$in" => values }
 				end
 			end
-		end
 
+			query_build
+		end
 end
