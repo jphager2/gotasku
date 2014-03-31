568fefb0f1c956e640c6e20b2820ee4da3e65cfa
jphager2 <jphager2@gmail.com>
2014-03-31 16:20:16 +0200

    cleaned code smell in parser and altered tests

--- lib/gotasku/parser.rb
+++ lib/gotasku/parser.rb
@@ -1,28 +1,30 @@
+# Parses Gotask::Problem sgf
 class Gotasku::Parser < SGF::Parser
+
+  @@regs = {AB: /AB/, AW: /AW/}
 
 	# validates and parses sgf string
 	def parse(sgf)
-    super(validate(sgf))	
+		super(self.class.validate(sgf))	
 	end
 
 	# validates sgf string for common goproblems.com issues
-	def validate(sgf)
-	  validate_for(validate_for(sgf, :AB), :AW)
+	def self.validate(sgf)
+		validate_for(validate_for(sgf, :AB), :AW)
 	end
 
 	# validates for a property (AB or AW)
-	def validate_for(sgf, property)
-		regs = {AB: /AB/, AW: /AW/}
-
-		# find the first instance, all will be replaced and the first 
-		# instance will be readded
-		start = sgf =~ regs[property] 
+	def self.validate_for(sgf, property)
+		# find the first instance, all instances following 
+		# will be replaced and the first instance will be readded
+		property_regex = @@regs[property]
+		start = sgf =~ property_regex	
 
 		if start
-			sgf.gsub!(regs[property], '')
-			sgf = sgf[0...start] + property.to_s + sgf[start..-1]
+			sgf.gsub!(property_regex, '')
+			sgf[0...start] + property.to_s + sgf[start..-1]
+		else
+			sgf
 		end
-
-		sgf
 	end
 end
