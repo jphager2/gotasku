ac48f7310e615edf8825b9e54d36bf92fdd97020
jphager2 <jphager2@gmail.com>
2014-04-01 16:50:33 +0200

    cleaned up code smell in session

--- lib/gotasku/session.rb
+++ lib/gotasku/session.rb
@@ -1,3 +1,4 @@
+# login session for a user on goproblems.com
 class Gotasku::Session
 
 	@@current = nil
@@ -7,21 +8,21 @@ class Gotasku::Session
 	# uses mechanize to login to goproblems.com and makes makes this the
 	# current session
 	def initialize(user)
-		a = Mechanize.new
-		a.get('http://www.goproblems.com/') do |page|
+		mech = Mechanize.new
+		mech.get('http://www.goproblems.com/') do |page|
 			# Click the login link
-			login_page = a.click(page.link_with(href: /login/))
+			login_page = mech.click(page.link_with(href: /login/))
 
 			# Submit the login form
 			login_page.form_with(
-								action: 'login.php') do |f|
-				username_field = f.field_with(name: "name")
+								action: 'login.php') do |form|
+				username_field       = form.field_with(name: "name")
 				username_field.value = user.username
 				
-				password_field = f.field_with(name: "password")
+				password_field       = form.field_with(name: "password")
 				password_field.value = user.password 
 
-				@account = f.submit
+				@account = form.submit
 			end
 		end
 
